c489f273015cdf01ecb026250784128b6406f71d
Tony Y Liao <Tony.Y.Liao@BenQ.com>
2021-01-15 14:39:50 +0800

    feat: '引入normalize，定义全局样式'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "normalize.css";    // a modern alternative to CSS resets
+import "./styles/index.scss";   // global css
 
 Vue.config.productionTip = false;
 
